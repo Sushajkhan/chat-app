b33fd788e28609a3aa3edea6438ad79bed3baade
Sushajkhan <sushajkhan@gmail.com>
2024-03-07 20:50:29 +0530

    Update HomeNav.jsx

--- client/src/components/HomeNav.jsx
+++ client/src/components/HomeNav.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/logo.png";
 const HomeNav = () => {
   const { isAuthenticated } = useAuth();
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-8" alt="Logo" />
