9b017b0c3f29e6e52141dead6b5b078dc7bc57dd
Sushajkhan <sushajkhan@gmail.com>
2024-03-07 21:14:16 +0530

    ui fix

--- client/src/components/HomeNav.jsx
+++ client/src/components/HomeNav.jsx
@@ -16,22 +16,14 @@ const HomeNav = () => {
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link
             to={isAuthenticated ? "/chathome" : "/login"}
-<<<<<<< HEAD
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center   "
-=======
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
->>>>>>> 0898295c9445012693fc0b487b50c27e1e8d7947
           >
             {isAuthenticated ? "Home" : "Login"}
           </Link>
           <button
             data-collapse-toggle="navbar-cta"
             type="button"
-<<<<<<< HEAD
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200   "
-=======
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
->>>>>>> 0898295c9445012693fc0b487b50c27e1e8d7947
             aria-controls="navbar-cta"
             aria-expanded="false"
           >
@@ -57,19 +49,11 @@ const HomeNav = () => {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
         >
-<<<<<<< HEAD
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white   ">
             <li>
               <Link
                 to="/about"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700    "
-=======
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white 
-            <li>
-              <Link
-                to="/about"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
->>>>>>> 0898295c9445012693fc0b487b50c27e1e8d7947
               >
                 About
               </Link>
@@ -78,11 +62,7 @@ const HomeNav = () => {
             <li>
               <Link
                 to="/contact"
-<<<<<<< HEAD
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700  "
-=======
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 "
->>>>>>> 0898295c9445012693fc0b487b50c27e1e8d7947
               >
                 Contact
               </Link>
